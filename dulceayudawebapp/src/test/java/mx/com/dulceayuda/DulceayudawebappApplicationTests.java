da85fcc8567331902427b668bb520cf0d2eca86a
Neri Sanchez Lozano <neri.sanchez@bancoazteca.com>
2020-07-07 17:13:43 -0500

    Proyecto inicial

---
+++ dulceayudawebapp/src/test/java/mx/com/dulceayuda/DulceayudawebappApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.dulceayuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DulceayudawebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
